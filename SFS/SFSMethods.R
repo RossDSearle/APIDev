3f7d7c3788401ca51c6fd4ac3d5e4b6534aa174c
Ross Searle <sea084@soils-discovery.it.csiro.au>
2019-06-13 12:31:13 +1000

    First deployment

--- SFS/SFSMethods.R
+++ SFS/SFSMethods.R
@@ -2,15 +2,19 @@ library(stringr)
 library(DBI)
 library(RSQLite)
 library(xts)
+library(gstat)
 
 regions <- read.csv(paste0(apiDevRootDir, '/SFS/SM_Regions.csv'), stringsAsFactors = F)
 
 
+
 doq <- function(sql){
+  con <- dbConnect(RSQLite::SQLite(), conPath, flags = SQLITE_RO)
   qry <- dbSendQuery(con, sql)
   res <- dbFetch(qry)
   dbClearResult(qry)
   #print(head(res))
+  dbDisconnect(con)
   return(res)
 }
 
@@ -18,28 +22,34 @@ doq <- function(sql){
 getRegionalSMMap <- function(region, dt){
   
   reg <- regions[regions$Region==region,]
+
+  smipsPath <- getSmips(reg, dt)
+  smipsR <- raster(smipsPath)
   
-  smipsR <- getSmips(reg, dt)
-  
-  probeData <- getProbeDataForaDate(dt)
-  
-  probePts <- drillProbeLocations(probeData, smipsR)
-  
+   probeData <- getProbeDataForaDate(dt)
+   
+   probePts <- drillProbeLocations(probeData, smipsR)
+
   outMapPath <- krigMap(probePts, smipsR)
-  
+
+  unlink(smipsPath)
+
   return(outMapPath)
+
 }
 
 
 
 drillProbeLocations <- function(probeData, smipsR){
   
+  #print(smipsR)
   coordinates(probeData) <-  ~Longitude+Latitude
   crs(probeData) <- CRS("+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0")
-  plot(probeData)
+  #plot(probeData)
   pts <- extract(smipsR, probeData)
   smipsDrill <- data.frame(probeData@coords[,1], probeData@coords[,2], probeData@data$SM, pts)
   probeData@data$smips <- pts
+  #print(head(probeData))
   return(probeData)
 }
   
@@ -49,14 +59,14 @@ krigMap <- function(probeData, smipsR){
   mod1 = lm(probeData$SM ~ probeData$smips, data = probeData)
   summary(mod1)
   
-  v <- automap::autofitVariogram(SM ~ smips,probeData)
+  v <- suppressWarnings(automap::autofitVariogram(SM ~ smips,probeData))
 
-  mMod <- gstat(NULL, "moist", SM ~ smips, probeData , model = v$var_model)
+  mMod <-  suppressWarnings(gstat(NULL, "moist", SM ~ smips, probeData , model = v$var_model))
   
   names(smipsR) <- 'smips'
   
   outFilePath <- paste0(apiDevRootDir, '/SFS/tmp/',  basename(tempfile()), '.tif')
-  sfsMap <- interpolate(smipsR, mMod, xyOnly = FALSE, index = 1,format="GTiff",overwrite=T)
+  sfsMap <- suppressWarnings( interpolate(smipsR, mMod, xyOnly = FALSE, index = 1,format="GTiff",overwrite=T))
   wrMsk <- raster(paste0(apiDevRootDir, '/SFS/Masks/SFS.tif'))
   
   om <- mask(sfsMap,wrMsk, filename=outFilePath)
@@ -69,14 +79,14 @@ getSmips <- function(reg, dt){
   yr<-str_sub(dt, 1,4)
   xsub <- paste0('&SUBSET=x(', reg$minx, ',', reg$maxx, ')')
   ysub <- paste0('&SUBSET=y(', reg$miny, ',', reg$maxy, ')')
-  outfile = paste0( 'C:/Projects/SMIPS/SFS/regionalSM/wetnessIndex/wi_', dt, '.tif' )
+  outfile = paste0(apiDevRootDir, '/SFS/tmp/',  basename(tempfile()), '.tif')
   wcsUrl <- paste0('http://ternsoils.nexus.csiro.au/cgi-bin/mapserv.exe?map=e:/MapServer/SMIPS/moisture.map&SERVICE=WCS&VERSION=2.0.0&REQUEST=GetCoverage&COVERAGEID=CSIRO_Wetness-Index&FORMAT=image/tiff&mYear=', yr, '&mDate=', dt, '&mFName=CSIRO_Wetness-Index', xsub, ysub, '&FORMAT=image/tiff')
   download.file(wcsUrl, destfile = outfile, quiet = T, mode = 'wb')
   
-  wi <- raster(outfile)
-  crs(wi)
-  plot(wi)
-  return(wi)
+  #wi <- raster(outfile)
+  #crs(wi)
+  #plot(wi)
+  return(outfile)
 }
 
 
