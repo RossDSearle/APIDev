658649c0594e8da9f9dba8a395bb5f29053cc324
Ross Searle <sea084@soils-discovery.it.csiro.au>
2019-10-09 13:18:26 +1100

    sync

--- SFS/Validation.R
+++ SFS/Validation.R
@@ -23,36 +23,36 @@ writeOGR(sd, paste0(dataRoot, '/Validation'), 'SensorLocs', driver="ESRI Shapefi
 
          
 
-url <- 'http://esoil.io/SensorFederationWebAPI/SensorAPI/getSensorDataStreams?siteid=VicAg_LakeBolac&sensortype=Rainfall&startdate=2016-01-01T00%3A00%3A00&enddate=2019-01-01T00%3A00%3A00&aggperiod=days'
+url <- 'http://esoil.io/SensorFederationWebAPI/SensorAPI/getSensorDataStreams?siteid=VicAg_LakeBolac&sensortype=Rainfall&startdate=2016-01-01T00%3A00%3A00&enddate=2019-10-01T00%3A00%3A00&aggperiod=days'
 rainfall <- fromJSON(URLencode(url))
 
 rts <- xts(rainfall$DataStream[[1]]$v, order.by = as.Date(rainfall$DataStream[[1]]$t))
-bolacRain <- rts['2016']
+bolacRain <- rts['2016/2019']
 plot(bolacRain)
 
 
-saveRDS(bolacRain,  paste0(dataRoot, '/Validation/bolacRain.rds'))
+saveRDS(bolacRain,  paste0(dataRoot, version, '/Validation/bolacRain.rds'))
 
-url <- 'http://esoil.io/SensorFederationWebAPI/SensorAPI/getSensorDataStreams?siteid=VicAg_LakeBolac&sensortype=Soil-Moisture&startdate=2016-01-01T00%3A00%3A00&enddate=2019-01-01T00%3A00%3A00&aggperiod=days'
+url <- 'http://esoil.io/SensorFederationWebAPI/SensorAPI/getSensorDataStreams?siteid=VicAg_LakeBolac&sensortype=Soil-Moisture&startdate=2016-01-01T00%3A00%3A00&enddate=2019-10-01T00%3A00%3A00&aggperiod=days'
 sm <- fromJSON(URLencode(url))
 
 head(sm$DataStream[1])
 str(sm$DataStream)
 
 smts <- xts(sm$DataStream[[1]]$v, order.by = as.Date(sm$DataStream[[1]]$t))
-bolacSM30<- smts['2016']
+bolacSM30 <- smts['2016-01-01/2019-10-01']
 bolacSM30[which(bolacSM30 < 0)] <- NA
 plot(bolacSM30)
 
 bsm <-na.approx(bolacSM30)
 plot(bsm)
-saveRDS(bsm,  paste0(dataRoot, '/Validation/bolacSM30.rds'))
+saveRDS(bsm,  paste0(dataRoot,version, '/Validation/bolacSM30.rds'))
 
 
-SMIn <- paste0(dataRoot, '/Rasters/300')
+SMIn <- paste0(dataRoot,version, '/Rasters/300')
 
 sdate <- as.Date('2016-01-01')
-edate <- as.Date('2016-12-31')
+edate <- as.Date('2019-10-01')
 depth <- 300
 
 dts <- seq.Date(sdate, edate,1)
